81a035a9d1788caabd85aa4ac5089e1b9625c75b
BerlinRao <greentime55@hotmail.com>
2022-02-10 22:21:29 +0800

    password retry log in

---
+++ password.py
@@ -0,0 +1,13 @@
+password = 'a123456'
+i = 3
+while True:
+	p = input('請輸入密碼: ')
+	if password == p:
+		print('登入成功')
+		break
+	else:
+		i = i - 1
+		if i == 0:
+			break
+		print('密碼錯誤!還有', i, '次機會')
+		
