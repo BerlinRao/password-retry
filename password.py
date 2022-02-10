3ea16a2492b47de6e3a53aa7b69804d152736831
BerlinRao <greentime55@hotmail.com>
2022-02-10 22:51:30 +0800

    change to while i > 0

--- password.py
+++ password.py
@@ -1,13 +1,15 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
+	i = i - 1
 	p = input('請輸入密碼: ')
 	if password == p:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
-		if i == 0:
-			break
-		print('密碼錯誤!還有', i, '次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('您已經被鎖帳號了')
 		
